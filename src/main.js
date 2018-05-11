4adb6988031a9ccfb8ae89b3cb403fa32e56a24c
czero1995 <czero1995@gmail.com>
2018-05-11 18:00:54 +0800

    vconsole

--- src/main.js
+++ src/main.js
@@ -7,20 +7,25 @@ import VueAwesomeSwiper from 'vue-awesome-swiper'
 import axios from 'axios'
 import VueLazyLoad from 'vue-lazyload'
 import fastclick from 'fastclick'
+import Vconsole from 'vconsole'
+let vConsole = new Vconsole()
+export default vConsole
 var VueTouch = require('vue-touch')
 require('../mock')
 fastclick.attach(document.body) //解决移动端点击事件200ms延迟
 
 Vue.use(Vuex)
-Vue.prototype.$http=axios;
-Vue.use(VueTouch, {name: 'v-touch'}) //滑动事件
-VueTouch.config.swipe = {            //配置滑动区域为左滑动，解决和滚动事件的冲突
-	direction: 'horizontal',
-  	threshold: 200
+Vue.prototype.$http = axios;
+Vue.use(VueTouch, {
+  name: 'v-touch'
+}) //滑动事件
+VueTouch.config.swipe = { //配置滑动区域为左滑动，解决和滚动事件的冲突
+  direction: 'horizontal',
+  threshold: 200
 }
-Vue.config.productionTip = false      //vuex开启开发环境日志
+Vue.config.productionTip = false //vuex开启开发环境日志
 // or with options
-Vue.use(VueLazyLoad, {                //懒加载声明错误图和占位图
+Vue.use(VueLazyLoad, { //懒加载声明错误图和占位图
   preLoad: 1.3,
   error: './static/img/github.png',
   loading: './static/img/github.png',
@@ -31,5 +36,7 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
