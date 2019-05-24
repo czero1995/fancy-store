a9898d7c0e4fa864c9749fd928ee0a36eb8ad929
System Administrator <root@cpe-172-100-100-23.twcny.res.rr.com>
2019-05-24 18:05:36 +0800

    添加路由切换动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <transition name="router-fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 <script>
@@ -19,5 +21,13 @@ export default {
 @import "../public/less/base.less";
 #app {
     height: 100%;
+}
+.router-fade-enter-active,
+.router-fade-leave-active {
+    transition: opacity 0.1s;
+}
+.router-fade-enter,
+.router-fade-leave-active {
+    opacity: 0;
 }
 </style>
